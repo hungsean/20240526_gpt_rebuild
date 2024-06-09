dad9be188a363da6a842c8b15098b917212c2795
hungsean <hungsean0419@gmail.com>
2024-06-09 17:27:27 +0800

    修改menu command

--- src/function/command.py
+++ src/function/command.py
@@ -1,15 +1,21 @@
 from commands import test, token_len, translate
 
+def menu(str_argument: str):
+    for key in commands.keys():
+        print("- ", key)
+    return 0
+
 commands = {
     "test": test.index,
     "token_len": token_len.index,
-    "translate": translate.index
+    "translate": translate.index,
+    "menu": menu
 }
 
 def exec_commands(command_name: str, str_arguments: str):
-    if command_name == "menu":
-        menu()
-        return 0
+    # if command_name == "menu":
+    #     menu()
+    #     return 0
 
     for key, value in commands.items():
         if key == command_name:
@@ -22,7 +28,3 @@ def exec_commands(command_name: str, str_arguments: str):
 def get_commands():
     return commands
 
-def menu():
-    for key in commands.keys():
-        print("- ", key)
-    return 0
